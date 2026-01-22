1f0ebaed4c622ce08fd936a97a23bd752cdf156b
Erick Lemos da Silva <ericklemos2001@hotmail.com>
2026-01-22 19:49:24 -0300

    Revert "criação do post view"

--- mysite/blog/views/post_view.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpRequest
-from django.views import generic
-
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        return HttpRequest('Hello World')
-    
